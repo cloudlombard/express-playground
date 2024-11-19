9dac88f5a86f446a1beee6da0d220c74d435842a
Claudio Lombardi <cl.lombardi@reply.it>
2024-11-19 17:38:18 +0100

    feat: added cors / frontend integration

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const usersController = require('./controllers/users');
+const usersController = require("./controllers/users");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.use('/api', usersController);
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api", usersController);
 
 app.listen(8001, function () {
   console.log("App started on port 8001");
